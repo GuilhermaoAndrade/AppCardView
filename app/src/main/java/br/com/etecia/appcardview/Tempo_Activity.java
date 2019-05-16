9ed4d80f77a5621087fde437f219eb7755e27229
manager <guilherme.souza102@fatec.sp.gov.br>
2019-05-15 22:11:47 -0300

    criação do CardView Tempo

---
+++ app/src/main/java/br/com/etecia/appcardview/Tempo_Activity.java
@@ -0,0 +1,19 @@
+package br.com.etecia.appcardview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Tempo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tempo_layout);
+
+    }
+}
